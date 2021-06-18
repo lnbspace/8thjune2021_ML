ca38e93a5bdf29928958bbf3b7a60fe5a7e05cc0
Peeyush Sanam <peeyush.sanam@gmail.com>
2021-06-18 19:11:32 +0530

    Add files via upload

---
+++ shell_file.py
@@ -0,0 +1,177 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = input('Enter the value: ')
+Enter the value: 45
+>>> a
+'45'
+>>> type(a)
+<class 'str'>
+>>> a = int(input('Enter the value: '))
+Enter the value: 45
+>>> a
+45
+>>> type(a)
+<class 'int'>
+>>> a = float(input('Enter the value: '))
+Enter the value: 45.6
+>>> a
+45.6
+>>> a = eval(input('Enter the value: '))
+Enter the value: 45.3
+>>> a
+45.3
+>>> a = eval(input('Enter the value: '))
+Enter the value: 44
+>>> a
+44
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 45
+45 is Odd Number
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 66
+66 is even number
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 45
+Enter the second value: 23
+Enter the third value: 7
+a is bigger
+its largest
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 45
+Enter the second value: 45
+Enter the third value: 3
+this is not desired
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 4
+Enter the second value: 6
+Enter the third value: 7
+b is bigger
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 30
+Enter the second value: 27
+nice
+nice
+nice
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 5
+Enter the second value: 5
+Enter the third value: 5
+good
+good
+good
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 6
+Enter the second value: 4
+Enter the third value: 8
+good 6
+good 4
+good 8
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+[8, 7, 56, 4, 9, 11, 10]
+[7, 9, 11]
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+[8, 7, 56, 4, 9, 11, 10]
+[7, 9, 11]
+[4, 28, 2, 5]
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 39
+39
+78
+117
+156
+195
+234
+273
+312
+351
+390
+>>> 
+========= RESTART: E:/TN/SITP21/8thJune_B1_AI_ML/day8/script_file.py ========
+Enter the value: 47
+47
+94
+141
+188
+235
+282
+329
+376
+423
+470
+********************
+470
+423
+376
+329
+282
+235
+188
+141
+94
+47
+>>> list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> list(range(4,10))
+[4, 5, 6, 7, 8, 9]
+>>> 
+========= RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ========
+Enter the value: 54
+
+hi
+hihi
+hihihi
+hihihihi
+>>> 
+========= RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ========
+
+*
+**
+***
+****
+******
+*******
+*********
+>>> print(4,5,6,7,8,9)
+4 5 6 7 8 9
+>>> print(4,5,6,7,8,9,sep='$')
+4$5$6$7$8$9
+>>> print(4,5,6, sep='$', end='%')
+4$5$6%
+>>> 
+========= RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ========
+4$5$6%done
+>>> 
+========= RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ========
+4$5$6
+done
+>>> # ternary conditional statement
+>>> print('its odd') if eval(input('Enter the number: '))%2 else print('its even')
+Enter the number: 7
+its odd
+>>> 
+=============== RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ==============
+[4, 5, 7, 8, 93, 3]
+[8, 10, 14, 16, 186, 6]
+>>> 
+=============== RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ==============
+[4, 5, 7, 8, 93, 3]
+[8, 10, 14, 16, 186, 6]
+[5, 7, 93, 3]
+>>> 
+=============== RESTART: E:\TN\SITP21\8thJune_B1_AI_ML\day8\script_file.py ==============
+[4, 5, 7, 8, 93, 3]
+[8, 10, 14, 16, 186, 6]
+[5, 7, 93, 3]
+[2, 5, 7, 4, 93, 3]
+>>> 
